3c08497f2bdd3202d7ee5120f9a46ad8fc38c1c0
Luis Fernando Calegari <calegariluisfernando@gmail.com>
2020-04-14 22:36:01 -0300

    Estudo-3 | Bootstrap

--- teste-react-bootstrap/src/index.js
+++ teste-react-bootstrap/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+/**
+ * Importado assim apenas para ver o Webpack trabalhando.
+ * Nota "Top" pra caralho.
+ */
+import 'bootstrap/scss/bootstrap.scss';
 
 import App from './App';
 
